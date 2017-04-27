620e7249fbff38f1a57b9b8bcb2c94b89ec1c775
Preston Matheson <pmphoto123@gmail.com>
2017-04-27 17:00:00 -0600

    added initial stylings for the blogs view page and the connection between the index and show pages

--- app/controllers/blogs_controller.rb
+++ app/controllers/blogs_controller.rb
@@ -1,7 +1,28 @@
 class BlogsController < ApplicationController
   def index
+    @blogs = Blog.all
   end
 
   def show
+    @blog = Blog.find(params[:id])
+
+    @page_title = @blog.title
+    @seo_keywords = @blog.body
+  end
+
+  def new
+  end
+
+  def create
+    @blog = Blog.new(params[:blog].permit(blog_params))
+
+    @blog.save
+    redirect_to @blog
+  end
+
+  private 
+
+  def blog_params
+    params.require(:blog).permit(:title, :body, :topic_id, :status)
   end
 end
--- app/models/blog.rb
+++ app/models/blog.rb
@@ -1,2 +1,4 @@
 class Blog < ApplicationRecord
+  validates_presence_of :title, 
+                        :body
 end
